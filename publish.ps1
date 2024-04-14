12237d0e8e23178d32cd5d69b69bcc19f0a115d3
勇哥 <ygman@qq.com>
2024-04-14 17:02:20 +0800

    update: net8.0

--- publish.ps1
+++ publish.ps1
@@ -3,12 +3,12 @@ Set-Location $PSScriptRoot
 $version=$csproj.Project.PropertyGroup.Version
 
 dotnet publish src -c release -f net472 -o publish\net472
-dotnet publish src -c release -f net6.0-windows -o publish\net6.0-windows
-dotnet publish src -c release -f net6.0-windows -o publish\net6.0-windows-x64 -r win-x64 --self-contained=false -p:PublishSingleFile=true
+dotnet publish src -c release -f net8.0-windows -o publish\net8.0-windows
+dotnet publish src -c release -f net8.0-windows -o publish\net8.0-windows-x64 -r win-x64 --self-contained=false -p:PublishSingleFile=true
 
 Compress-Archive publish\net472\* -DestinationPath "publish\BahaHcaptcha_v$($version)_net472.zip" -f
-Compress-Archive publish\net6.0-windows\* -DestinationPath "publish\BahaHcaptcha_v$($version)_net6.0.zip" -f
-Compress-Archive publish\net6.0-windows-x64\* -DestinationPath "publish\BahaHcaptcha_v$($version)_net6.0_x64.zip" -f
+Compress-Archive publish\net8.0-windows\* -DestinationPath "publish\BahaHcaptcha_v$($version)_net8.0.zip" -f
+Compress-Archive publish\net8.0-windows-x64\* -DestinationPath "publish\BahaHcaptcha_v$($version)_net8.0_x64.zip" -f
 
-explorer.exe /select,"BahaHcaptcha_v$($version)_net472.zip"
+# explorer.exe /select,"BahaHcaptcha_v$($version)_net472.zip"
 Set-Location $PSScriptRoot
